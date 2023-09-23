84a1d902cbe531db0903fe9ee939f4aa007394cb
ItsRahul007 <rg890458@gmail.com>
2023-09-24 00:51:30 +0530

    server complete

--- dist/index.js
+++ dist/index.js
@@ -14,21 +14,6 @@ const io = new socket_io_1.Server(server, {
     },
     serveClient: false
 });
-// Checking the matchs of user's given text
-function matches(msg) {
-    if (msg.match("joke")) {
-        return "Joke";
-    }
-    else if (msg.match(/summary|blog|article/)) {
-        return "Artical";
-    }
-    else if (msg.match("news")) {
-        return "News";
-    }
-    else
-        return "Sorry sir, I have no knowledge about that. Please ask me anything else";
-}
-;
 io.on('connection', (socket) => {
     socket.on("happy", () => {
         socket.emit("mood", { msg: "Happy to hear, How can I help you?" });
@@ -38,10 +23,6 @@ io.on('connection', (socket) => {
     });
     socket.on("sad", () => {
         socket.emit("mood", { msg: "How can I help you to better your mood?" });
-    });
-    socket.on("send-msg", (msg) => {
-        const replay = matches(msg.toLowerCase());
-        socket.emit("response", { msg: replay });
     });
     socket.on('disconnect', () => {
         console.log('User disconnected');
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ import { ServerToClientEvents, ClientToServerEvents, InterServerEvents, SocketDa
 const app = express();
 const server = http.createServer(app);
 const io = new Server<
-ClientToServerEvents,
-ServerToClientEvents,
-InterServerEvents,
-SocketData
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
 >(server, {
   cors: {
     origin: "*"
@@ -17,40 +17,18 @@ SocketData
   serveClient: false
 });
 
-// Checking the matchs of user's given text
-function matches(msg: string){
-  if(msg.match("joke")){
-    return "Joke";
-  }
-
-  else if(msg.match(/summary|blog|article/)){
-    return "Artical";
-  }
-
-  else if(msg.match("news")) {
-    return "News";
-  }
-
-  else return "Sorry sir, I have no knowledge about that. Please ask me anything else"
-};
-
 io.on('connection', (socket: Socket) => {
 
-  socket.on("happy", ()=>{
-    socket.emit("mood", {msg: "Happy to hear, How can I help you?"});
+  socket.on("happy", () => {
+    socket.emit("mood", { msg: "Happy to hear, How can I help you?" });
   });
 
-  socket.on("avarage", ()=>{
-    socket.emit("mood", {msg: "How can I help you to better your mood?"});
+  socket.on("avarage", () => {
+    socket.emit("mood", { msg: "How can I help you to better your mood?" });
   });
 
   socket.on("sad", () => {
     socket.emit("mood", { msg: "How can I help you to better your mood?" });
-  });
-
-  socket.on("send-msg", (msg: any) => {
-    const replay = matches(msg.toLowerCase());
-    socket.emit("response", {msg: replay});
   });
 
   socket.on('disconnect', () => {
